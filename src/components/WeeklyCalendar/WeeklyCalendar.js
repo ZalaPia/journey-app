c235c5120a0623ca91d8cecbc10284f36bcd4c39
ZalaPia <zalapia.lipicnik@gmail.com>
2026-02-27 21:33:54 +0100

    WeeklyCalendar component simple structure

--- src/components/WeeklyCalendar/WeeklyCalendar.js
+++ src/components/WeeklyCalendar/WeeklyCalendar.js
@@ -1,5 +1,6 @@
 import { DayCell } from '../DayCell/DayCell';
 import styles from './WeeklyCalendar.module.scss';
+import {Button} from "../Button/Button";
 
 function sameDay(a, b) {
     return (
@@ -26,17 +27,55 @@ export function WeeklyCalendar({ weekDates, selectedDate, onSelectDate }) {
                 ))}
             </div>
 
-            <article className={styles.dayDetail}>
-                <h3>
-                    {selectedDate.toLocaleDateString('en-GB', { weekday: "long" , day: '2-digit', month: 'short', year: 'numeric' })}
+            <section className={styles.dayDetail}>
+                <h3 className={styles.dateTitle}>
+                    {selectedDate.toLocaleDateString("en-GB", {
+                        weekday: "long",
+                        day: "2-digit",
+                        month: "long",
+                        year: "numeric",
+                    })}
                 </h3>
-                <ul>
-                    <li>Symptoms</li>
-                    <li>Important dates</li>
-                    <li>Thoughts, Ideas and Quotes</li>
-                    <li>Sticky notes</li>
-                </ul>
-            </article>
+                <p className={styles.subtitle}>
+                    All entries for this day
+                </p>
+
+                <div className={styles.entriesGrid}>
+
+                            <div className={styles.entryBox}>
+                                <h4 className={styles.symptoms}>Symptoms</h4>
+                                <hr className={styles.symptoms__line}/>
+                                <textarea placeholder="Add symptoms…" />
+                                <Button variant="black">
+                                    +
+                                </Button>
+                            </div>
+
+                            <div className={styles.entryBox}>
+                                <h4 className={styles.important_dates}>Important dates</h4>
+                                <hr className={styles.important_dates__line} />
+                                <textarea placeholder="Add important dates…" /><Button variant="black">
+                                + Add
+                            </Button>
+                            </div>
+
+                            <div className={`${styles.entryBox} ${styles.fullWidth}`}>
+                                <h4 className={styles.thoughts_ideas}>Thoughts, Ideas and Quotes</h4>
+                                <hr className={styles.thoughts_ideas__line}/>
+                                <textarea placeholder="Write your thoughts…" />
+                            </div>
+
+                            <div className={`${styles.entryBox} ${styles.fullWidth}`}>
+                                <h4 className={styles.sticky_notes}>Sticky notes</h4>
+                                <hr className={styles.sticky_notes__line}/>
+                                <textarea placeholder="Add notes…" />
+                                <Button variant="black">
+                                + Add
+                                </Button>
+                            </div>
+
+                </div>
+            </section>
         </section>
     );
 }
